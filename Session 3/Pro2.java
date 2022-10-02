cd956a24262e9a7f5309c0524500e56fb87b137c
Dilshard <mrdilshard@gmail.com>
2022-10-02 16:43:56 +0530

    Session 03

---
+++ Session 3/Pro2.java
@@ -0,0 +1,11 @@
+class Pro2
+{
+   public static void main(String[] arg)
+   {
+	System.out.println("First Name\n: John");
+	System.out.println("Last Name\t: Peter");
+	System.out.println("Email\t\t: Peter@\ngmail.com");
+	System.out.println("Gender\t\t: Male");
+	System.out.println("Mobile\t\t: 01234567");
+   }
+} 
